d8f6e07f69be3b7c8196d9b402242cd647f2d0e5
robonoriaki <s1526038GQ@s.chibakoudai.jp>
2020-01-15 14:15:56 +0900

    Add a contact flag

--- test_walker1/simulator.py
+++ test_walker1/simulator.py
@@ -1,16 +1,23 @@
 import pybullet as p
 import time
 import pybullet_data
-import numpy
+import math
+
+PI = math.pi
 
 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version 
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally 
+#set Gravity
 p.setGravity(0,0,-9.8)
+#load plane URDF
 planeId = p.loadURDF("plane.urdf")
+#initial position of a robot
 cubeStartPos = [0,0,1]
+#initial orientation of a robot
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-robotId = p.loadURDF("urdf/test_walker1.urdf",cubeStartPos, cubeStartOrientation)
+#load a robot
+robotId = p.loadURDF("urdf/test_walker1.urdf", cubeStartPos, cubeStartOrientation)
 
 mode = p.POSITION_CONTROL
 angle = 0
@@ -18,7 +25,23 @@ add_angle = 3.14 / 50
 jindex = 0
 count = -30
 
+simpara = p.getPhysicsEngineParameters(physicsClient)
+print('simulation parameters')
+print(simpara)
+
 for i in range (10000):
+    #get Base position
+    basePos, baseQua = p.getBasePositionAndOrientation(robotId)
+    
+    #VRPos = (basePos[0] + 0.2, basePos[1] - 0.5, basePos[0] + 0.2)
+    #VRQua = (baseQua[0], baseQua[1], baseQua[2])
+    #VRtarget = (basePos[0], basePos[1], basePos[2])
+    #print('VRPos type')
+    #print(type(VRPos))
+    #set Camera state
+    #p.setVRCameraState(VRPos, VRQua, VRtarget)
+    p.resetDebugVisualizerCamera(2.0, 10, -30, basePos)
+
     count = count + 1;
     if count < 0:
         angle = angle + add_angle
@@ -41,11 +64,23 @@ for i in range (10000):
     #Get joint information
     jointinfo = p.getJointInfo(robotId, jindex)
     #print(jointinfo[1])
+    
+    #get contact point
+    conflag = p.getContactPoints(robotId, planeId, 1)
+    #print('contact flag')
+    #print(conflag)
+
+   
+    conflen = len(conflag)
+    if conflen != 0:
+        print('conflag')
+        print(conflag[0][0])
 
     #display
     p.stepSimulation()
-    time.sleep(1./2400.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(robotId)
-print(cubePos,cubeOrn)
+    #time.sleep(1./100000.)
+
+    #basePos, baseQua = p.getBasePositionAndOrientation(robotId)
+    #print(basePos,baseQua)
 p.disconnect()
 
